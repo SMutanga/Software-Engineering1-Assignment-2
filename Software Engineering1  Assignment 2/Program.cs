d39d04c77a2a7aed501a598acb780c07af0dcefc
Sannah Penelope Azuh Mutanga <penelopemutanga@outlook.com>
2025-08-27 15:51:18 -0500

    Made a calculator that divides two numbers.

--- Software Engineering1  Assignment 2/Program.cs
+++ Software Engineering1  Assignment 2/Program.cs
@@ -12,3 +12,13 @@ Console.WriteLine("The sum is: " + sum);
 
 
 Console.WriteLine("My name is Sannah Penelope Mutanga, and this is my project.");
+
+Console.Write("Enter first number: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+double num4 = Convert.ToDouble(Console.ReadLine());
+
+double quotient = num3 / num4;
+
+Console.WriteLine("The quotient is:" +quotient);
