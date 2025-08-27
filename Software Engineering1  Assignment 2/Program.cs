a742592149468163cf062d5bdf92b4d14bc30b2d
Sannah Penelope Azuh Mutanga <penelopemutanga@outlook.com>
2025-08-27 15:44:07 -0500

    Made a calculator that can add two numbers together.

--- Software Engineering1  Assignment 2/Program.cs
+++ Software Engineering1  Assignment 2/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+ Console.Write("Enter first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double sum = num1 + num2;
+
+Console.WriteLine("The sum is: " + sum);
